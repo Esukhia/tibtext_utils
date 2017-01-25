727ddee355f313922759931a4996f8198310c744
drupchen <hhdrupchen@gmail.com>
2017-01-25 17:10:10 +0100

    sketch of a lemmatizer

---
+++ lemmatizer/README.md
@@ -0,0 +1,3 @@
+# Lemmatizer
+
+One day a full lemmatizer, now generates ressources for lemmatizing.
--- lemmatizer/lemmatizer.py
+++ lemmatizer/lemmatizer.py
@@ -2,11 +2,12 @@ import os
 import re
 from collections import defaultdict
 import simplejson as json
-from PyTib.common import open_file
+from PyTib.common import open_file, write_file, tib_sort
 
 
 def gen_verb_families():
-    monlam = json.loads(open_file('resources/monlam_verbs.json'))
+    raw = open_file('resources/monlam_verbs.json')
+    monlam = json.loads(raw)
     # generate the structure for all
     all_families = defaultdict(list)
     for form, lemmas in monlam.items():
@@ -26,5 +27,23 @@ def gen_verb_families():
     return families
 
 
+def gen_particle_families():
+    particles = json.loads(open_file('resources/particles.json'))
+    return particles
+
+
+def format_families(families):
+    lemmas_sorted = tib_sort(list(families.keys()))
+    formatted = []
+    for lemma in lemmas_sorted:
+        members = families[lemma]
+        formatted.append('{}:    {}'.format(lemma, '    '.join(members)))
+    return '\n'.join(tib_sort(formatted))
+
 verb_families = gen_verb_families()
+verbs_formatted = format_families(verb_families)
+
+particle_families = gen_particle_families()
+particle_formatted = format_families(particle_families)
+write_file('output/lemmas.txt', particle_formatted+'\n'+verbs_formatted)
 print('ok')
