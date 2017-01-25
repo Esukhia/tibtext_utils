7de21b0aadf7c13fd4d8112ea2880cb71ed2b68d
drupchen <hhdrupchen@gmail.com>
2017-01-25 14:40:57 +0100

    stemmer finished

--- stemmer/stemmer.py
+++ stemmer/stemmer.py
@@ -8,13 +8,16 @@ def pre_processing(string):
     """
     do all the pre_processing here
     :param string:
-    :return:
+    :return: a list of tokens
     """
+    # Todo: make all the input files homogeneous through operations in this function
     # separate the affixed particles into individual words
     string = string.replace('-', ' -')
+    string = string.replace('།', '')
     string = string.replace('\u2005', ' -')
     string = string.replace('\n', '')
-    return string
+    string = re.sub(r'\s+', ' ', string)
+    return string.split(' ')
 
 
 def find_raw_freqs(word_list):
@@ -29,6 +32,7 @@ def parse_families():
     parses families.txt into a dict with lists in the value slot
     :return:
     """
+    # Todo: change the input format and adapt this function
     content = open_file('families.txt')
     groups = {}
     for line in content.strip().split('\n'):
@@ -42,35 +46,83 @@ def parse_families():
 def find_families(raw_freqs):
     groups = parse_families()
 
-    inversed_groups = {}
-    for lemma, members in groups.items():
-        inversed_groups[tuple(members)] = lemma
-
     headwords = defaultdict(dict)
-    for word in raw_freqs:
-        for members, lemma in inversed_groups.items():
+    for word, freq in raw_freqs.items():
+        has_a_family = False
+        for lemma, members in groups.items():
             if word in members:
-                pass
-                #headwords[lemma][word]
+                headwords[lemma][word] = freq
+                has_a_family = True
+
+        # if the headword is a loner
+        if not has_a_family:
+            headwords[word][word] = freq
+    return headwords
 
 
 def create_headwords(word_list):
     raw_frequencies = find_raw_freqs(word_list)
     regrouped_frequencies = find_families(raw_frequencies)
+    return regrouped_frequencies
 
 
-def stem(input):
+def gen_total_types(input):
+    def update_total(lemma, family):
+        for member, freq in family.items():
+            total_headwords[lemma][member] += freq
+    total_headwords = {}
     for f in os.listdir(input):
         content = open_file('{}/{}'.format(input, f))
-        content = pre_processing(content)
-        segmented = content.split(' ')
-        # apply regexes
-        headwords = create_headwords(segmented)
+        segmented = pre_processing(content)
+        current_headwords = create_headwords(segmented)
+        for lemma, family in current_headwords.items():
+            if lemma not in total_headwords:
+                total_headwords[lemma] = defaultdict(int)
+                update_total(lemma, family)
+            else:
+                update_total(lemma, family)
+    return total_headwords
 
-        pass
+
+def gen_headwords_freq(member_freq):
+    headwords = []
+    for lemma, family in member_freq.items():
+        if len(family.keys()) == 1:
+            headwords.append([(lemma, family[lemma])])
+        else:
+            freq = sum(list(family.values()))
+            headword = (lemma, freq)
+            sorted_family = sorted([(member, m_freq) for member, m_freq in family.items()], key=lambda x: x[1])
+            headwords.append([headword]+sorted_family)
+    return headwords
+
+
+def generate_header(freqs):
+    longest = 0
+    for el in freqs:
+        if len(el) > longest:
+            longest = len(el)
+    lemma_header = ['stem', 'freq']
+    members_header = []
+    for a in range(longest):
+        members_header.append('word' + str(a))
+        members_header.append('freq')
+    return lemma_header+members_header
+
+
+def flatten_freq_struct(freqs):
+    flattened = []
+    for row in freqs:
+        flattened.append([b for a in row for b in a])
+    return flattened
+
 
 def main():
     in_path = 'input'
-    stemmed = stem(in_path)
+    members_freq = gen_total_types(in_path)
+    headwords_freqs = gen_headwords_freq(members_freq)
+    flattened = flatten_freq_struct(headwords_freqs)
+    header = generate_header(headwords_freqs)
+    write_csv('output/total_freqs.csv', flattened, header=header)
 
 main()
