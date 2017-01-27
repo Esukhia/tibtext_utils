4795e30969068a3c5bd816582aa55347f488fa3e
drupchen <hhdrupchen@gmail.com>
2017-01-27 17:21:53 +0100

    update

--- pre_processing/pre_processing.py
+++ pre_processing/pre_processing.py
@@ -1,28 +1,32 @@
 import re
 
+
 def clean_string(string,
+                 # 1. Replacements
                  tabs2spaces=False, under2spaces=False, spaces2same=False,
+                 # 2. Reducing to one element
                  single_spaces=False, single_returns=False, single_unders=False,
+                 # 3. Delete any given elements
                  del_spaces=False, del_returns=False, del_dashes=False,
+                 # 4. strips
                  l_strip=False, r_strip=False, strip=False, ):
 
-
-    # Replacements
+    # 1.
     if tabs2spaces: string = string.replace('\t', ' ')
     if under2spaces: string = string.replace('_', ' ')
     if spaces2same: string = re.sub(r'\s', ' ', string)
 
-    # Reducing to one element
+    # 2.
     if single_spaces: string = re.sub(r' +', r' ', string)
     if single_returns: string = re.sub(r'\n+', r'\n', string)
     if single_unders: string = re.sub(r'_+', r'_', string)
 
-    # Delete the given elements
+    # 3.
     if del_spaces: string = string.replace(' ', '')
     if del_returns: string = string.replace('\n', '')
     if del_dashes: string = string.replace('-', '')
 
-    # strips
+    # 4.
     if l_strip: string = string.lstrip()
     if r_strip: string = string.rstrip()
     if strip: string = string.strip()
